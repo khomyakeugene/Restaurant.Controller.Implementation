57dee63a7df7ddf385b159cb0c87910f732c520b
khomyakeugene <khomyak_eugene@yahoo.com>
2016-07-04 10:12:23 +0300

    Restaurant Controller Implementation

--- src/main/java/com/company/restaurant/controllers/EmployeeControllerImpl.java
+++ src/main/java/com/company/restaurant/controllers/EmployeeControllerImpl.java
@@ -2,14 +2,30 @@ package com.company.restaurant.controllers;
 
 import com.company.restaurant.dao.EmployeeDao;
 import com.company.restaurant.dao.JobPositionDao;
-import com.company.restaurant.model.Employee;
-import com.company.restaurant.model.JobPosition;
+import com.company.restaurant.model.*;
+import com.company.util.DataIntegrityException;
 
 import java.util.List;
+import java.util.Set;
 
 public class EmployeeControllerImpl implements EmployeeController {
+    private static final String OPERATION_IS_NOT_SUPPORTED_PATTERN = "<%s>: operation is not supported for <employee> with id <%d>";
+    private static final String THERE_IS_NOT_EMPLOYEE_WITH_ID_PATTERN = "There is not employee with id <%d>";
+
     private JobPositionDao jobPositionDao;
     private EmployeeDao employeeDao;
+
+    private void errorMessage(String message) {
+        throw new DataIntegrityException(message);
+    }
+
+    private void operationIsNotSupportedMessage(String message, int employeeId) {
+        errorMessage(String.format(OPERATION_IS_NOT_SUPPORTED_PATTERN, message, employeeId));
+    }
+
+    private void employeeNotFoundMessage(int employeeId) {
+        errorMessage(String.format(THERE_IS_NOT_EMPLOYEE_WITH_ID_PATTERN, employeeId));
+    }
 
     public void setJobPositionDao(JobPositionDao jobPositionDao) {
         this.jobPositionDao = jobPositionDao;
@@ -80,7 +96,52 @@ public class EmployeeControllerImpl implements EmployeeController {
     }
 
     @Override
-    public Employee findEmployeeById(int id) {
-        return employeeDao.findEmployeeById(id);
+    public Employee findEmployeeById(int employeeId) {
+        return employeeDao.findEmployeeById(employeeId);
+    }
+
+    private Employee getEmployeeById(int employeeId) {
+        Employee employee = employeeDao.findEmployeeById(employeeId);
+        if (employee == null) {
+            employeeNotFoundMessage(employeeId);
+        }
+
+        return employee;
+    }
+
+    @Override
+    public Set<Order> getEmployeeOrders(int employeeId) {
+        Set<Order> result = null;
+
+        Employee employee = getEmployeeById(employeeId);
+        if (employee instanceof Waiter) {
+            result = ((Waiter) employee).getOrders();
+
+        } else if (employee instanceof CookAndWaiter) {
+            result = ((CookAndWaiter) employee).getOrders();
+
+        } else {
+            operationIsNotSupportedMessage("EmployeeControllerImpl.getEmployeeOrders", employeeId);
+        }
+
+        return result;
+    }
+
+    @Override
+    public Set<CookedCourse> getEmployeeCookedCourses(int employeeId) {
+        Set<CookedCourse> result = null;
+
+        Employee employee = getEmployeeById(employeeId);
+        if (employee instanceof Cook) {
+            result = ((Cook) employee).getCookedCourses();
+
+        } else if (employee instanceof CookAndWaiter) {
+            result = ((CookAndWaiter) employee).getCookedCourses();
+
+        } else {
+            operationIsNotSupportedMessage("EmployeeControllerImpl.getEmployeeCookedCourses", employeeId);
+        }
+
+        return result;
     }
 }
